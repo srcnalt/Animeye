4a9ac377e1d47f22abbd945eb08ec9c45c84a693
Sarge <sercan.altundas@gmail.com>
2016-12-16 02:56:07 +0200

    gui and anim location
    User interface improved using SUIT lib for love2d, for now only sliders
    are used and each input has a range restriction.
    Animation is centered to right area, when scaled stays in the center.

---
+++ conf.lua
@@ -0,0 +1,5 @@
+function love.conf(t)
+    t.version =      "0.10.2"
+    t.window.title = "Animeye"
+    t.window.icon =  "icon.png"
+end
--- main.lua
+++ main.lua
@@ -2,9 +2,7 @@ local image_path = ""
 local image = {}
 local anim  = {}
 
-local width = 128
 local speed = 0.1
-local frame = 4
 local scale = 2
 
 local time  = 0
@@ -12,42 +10,97 @@ local last_modtime = 0
 
 local created  = false
 
+local vars = {
+	width = 128,
+	frame = 4,
+	speed = 0.1,
+	scale = 1
+}
+
+local suit   = require 'suit'
+local width_slider = {value = vars.width, min = 1, max = 256}
+local frame_slider = {value = vars.frame, min = 1, max = 32}
+local speed_slider = {value = vars.speed, min = 0, max = 2}
+local scale_slider = {value = vars.scale, min = 0, max = 5}
+
 function love.load()
 	require 'Anim'
 
+	drop = love.graphics.newImage('drop.png')
+
 	love.graphics.setDefaultFilter('nearest', 'nearest')
+	love.graphics.setBackgroundColor(65,77,108, 255)
+	love.window.setMode(800, 600, {fullscreen = false, centered = true})
 end
 
 function love.update(dt)
 	if created then
 		anim:update(dt)
-		checkFile(dt)
+		checkChange(dt)
 	end
+
+    suit.Label("Frame width: " .. math.floor(width_slider.value), {align = "left"}, 20, 10, 200, 20)
+    suit.Slider(width_slider, 20, 30, 250, 20)
+
+    suit.Label("Frame count: " .. math.floor(frame_slider.value), {align = "left"}, 20, 60, 200, 20)
+    suit.Slider(frame_slider, 20, 80, 250, 20)
+
+    suit.Label("Animation speed: " .. math.floor(speed_slider.value * 100) * 0.01, {align = "left"}, 20, 110, 200, 20)
+    suit.Slider(speed_slider, 20, 130, 250, 20)
+
+    suit.Label("Scale factor: " .. math.floor(scale_slider.value * 10) * 0.1, {align = "left"}, 20, 160, 200, 20)
+    suit.Slider(scale_slider, 20, 180, 250, 20)
 end
 
 function love.draw()
 	if created then
-		anim:draw(300, 100, 0, scale, scale)
-		love.graphics.print(last_modtime, 300, 10)
+		anim:draw(550 - vars.width * vars.scale / 2, 300 - image:getHeight() * vars.scale / 2, 0, vars.scale, vars.scale)
+	else
+		love.graphics.draw(drop, 500, 250)
 	end
 
-	love.graphics.print("1: Width up, 2: Width down", 5, 10)
-	love.graphics.print("Width of one frame: " .. width, 5, 25)
+	love.graphics.setColor(20, 30, 45, 255)
+	love.graphics.rectangle('fill', 0, 0, 300, 800)
+	love.graphics.setColor(255, 255, 255, 255)
 
-	love.graphics.print("3: Speed up, 4: Speed down", 5, 50)
-	love.graphics.print("Speed of animation: " .. speed, 5, 65)
+	suit.draw()
 
-	love.graphics.print("5: Frame up, 6: Frame down", 5, 90)
-	love.graphics.print("Number of frames: " .. frame, 5, 105)
+end
 
-	love.graphics.print("7: Scale up, 8: Scale down", 5, 130)
-	love.graphics.print("Scale factor: " .. scale, 5, 145)
+function checkChange(dt)
+	time = time + dt
+
+	if time > 1 then
+		modtime, errormsg = love.filesystem.getLastModified(image_path)
+
+		if last_modtime == 0 then
+			last_modtime = modtime
+		elseif modtime > last_modtime or checkVars() then
+			createImage(image_path)
+			last_modtime = modtime
+		end
+
+		time = 0
+	end
+end
+
+function checkVars()
+	if vars.width ~= width_slider.value or vars.frame ~= frame_slider.value or vars.scale ~= scale_slider.value or vars.speed ~= speed_slider.value then
+		vars.width = width_slider.value
+		vars.frame = frame_slider.value
+		vars.scale = scale_slider.value
+		vars.speed = speed_slider.value
+
+		return true
+	else
+		return false
+	end
 end
 
 function createImage(i)
-    img = love.graphics.newImage(i)
-	local frame = math.ceil(img:getWidth() / width)
-	anim  = newAnimation(img, width, img:getHeight(), speed, frame)
+    image = love.graphics.newImage(i)
+	local frame = math.ceil(image:getWidth() / vars.width)
+	anim  = newAnimation(image, vars.width, image:getHeight(), vars.speed, vars.frame)
 
 	created = true
 end
@@ -61,40 +114,9 @@ function love.filedropped(file)
 end
 
 function love.keypressed(key)
-	if love.keyboard.isDown(3) then
-		speed = speed - 0.05
-	elseif love.keyboard.isDown(4) then
-		speed = speed + 0.05
-	elseif love.keyboard.isDown(1) then
-		width = width + 1
-	elseif love.keyboard.isDown(2) and width > 1 then
-		width = width - 1
-	elseif love.keyboard.isDown(5) then
-		frame = frame + 1
-	elseif love.keyboard.isDown(6) and frame > 1 then
-		frame = frame - 1
-	elseif love.keyboard.isDown(7) then
-		scale = scale + 0.5
-	elseif love.keyboard.isDown(8) and scale > 1 then
-		scale = scale - 0.5
-	end
-
-	anim = newAnimation(img, width, img:getHeight(), speed, frame)
+	suit.keypressed(key)
 end
 
-function checkFile(dt)
-	time = time + dt
-
-	if time > 1 then
-		modtime, errormsg = love.filesystem.getLastModified(image_path)
-
-		if last_modtime == 0 then
-			last_modtime = modtime
-		elseif modtime > last_modtime then
-			createImage(image_path)
-			last_modtime = modtime
-		end
-
-		time = 0
-	end
+function love.textinput(t)
+    suit.textinput(t)
 end
